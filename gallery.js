6fe3d0d00d15af412b894354e2393a23cd9219f4
Adi Čaušević <adi.causevic@protonmail.com>
2023-01-12 14:02:29 +0100

    Gallery functionality

--- gallery.js
+++ gallery.js
@@ -1,7 +1,25 @@
-let url = 'https://example.com';
 
-fetch(url)
-.then(res => res.json())
-.then(out =>
-  console.log('Checkout this JSON! ', out))
-.catch(err => { throw err });
+function populateGallery(data){
+    let container = document.getElementById("container");
+    data.forEach(element => {
+        const div = document.createElement("div");
+        div.className = "gallery-entry-container";
+        div.innerHTML = `
+                <div class="gallery-entry">
+                    <img src="${element.title_image}" class="entry-image">
+                    <a class="entry-overlay" href="${element.link}">
+                        ${element.title}
+                    </a>
+                </div>
+        `
+        container.appendChild(div);
+    });
+}
+
+onload = (event) => {
+    let url = 'https://raw.githubusercontent.com/ChocolateChipKookie/chocolatechipkookie.github.io/main/files/gallery_metadata.json';
+    fetch(url)
+        .then(res => res.json())
+        .then(populateGallery)
+        .catch(err => { throw err });
+}
